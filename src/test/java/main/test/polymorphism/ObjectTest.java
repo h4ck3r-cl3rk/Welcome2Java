c36b8dc2ca48f5f4c7efb93c298f54d55542b784
h4ck3r-cl3rk <admin@pizzahof.net>
2017-05-06 11:06:56 +0200

    added UnitTests

---
+++ src/test/java/main/test/polymorphism/ObjectTest.java
@@ -0,0 +1,19 @@
+package main.test.polymorphism;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ObjectTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
